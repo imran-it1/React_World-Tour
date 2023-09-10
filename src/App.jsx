2e4975ededed420e50ee8712c405c3c8b6c68801
imran-it1 <imranhossainsakil22@gmail.com>
2023-09-10 17:54:01 +0600

    Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Countries from "./components/Countries/Countries";
 function App() {
   return (
     <>
-      <h1 className="text-4xl text-center text-white">React World Tour</h1>
+      <h1 className="text-4xl text-center text-white">React Awesome World Tour!!!</h1>
       <Countries></Countries>
     </>
   );
