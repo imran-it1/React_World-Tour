31473a91bcedc5801ed3da2da55663aae8465785
imran-it1 <imranhossainsakil22@gmail.com>
2023-09-10 17:31:06 +0600

    Done

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -3,7 +3,14 @@ import Country from '../Country/Country';
 
 const Countries = () => {
 
-    const [countries, setCountries] = useState([])
+    // Countries State
+    const [countries, setCountries] = useState([]);
+    // Visited Countries State
+    const [visited, setVisited] = useState([])
+    // Visited Flags
+    const [visitedFlags, setVisitedFlags] = useState([])
+
+
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
@@ -11,15 +18,45 @@ const Countries = () => {
     },[])
 
 
+    const handleVisitedCountry = country => {
+        setVisited([...visited, country]);
+    }
+
+    const handleFlags = country => {
+        setVisitedFlags([...visitedFlags, country])
+    }
+
+
     return (
         <div>
+            {/* Countries */}
             <h1 className='text-center border border-purple-500 p-5 rounded-lg text-2xl mt-5'>Countries: {countries.length}</h1>
 
+            {/* Visited Countries Names*/}
+            <div className=' border-2 border-green-500 mt-5 p-5 rounded-md'>
+                <h1 className=' text-2xl mb-2 font-semibold'>Visited Countries: {visited.length}</h1>
+                <ol className=' list-decimal list-inside'>
+                    {
+                        visited.map(country => <li key={country}>{country.name.common}</li>)
+                    }
+                </ol>
+            </div>
+            {/* Visited Countries Flags */}
+            <div className=' border border-purple-500 p-5 mt-5 rounded-md'>
+                    <h1 className=' text-2xl font-semibold mb-2'>Flags: {visitedFlags.length}</h1>
+                    {
+                        visitedFlags.map(country => <img className='w-[100px] h-[70px] inline-block mr-5' src={country.flags.png}></img>)
+                    }
+            </div>
+
+            {/* Country Data / Component */}
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
                 {
                     countries.map(country => <Country
                     key={country.cca3}
                     country={country}
+                    handleVisitedCountry={handleVisitedCountry}
+                    handleFlags ={handleFlags}
                     ></Country>)
                 }
             </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,15 +1,31 @@
-const Country = ({country}) => {
-    const {name, flags, area, population, region} = country;
-    console.log(country);
+import { useState } from "react";
+
+const Country = ({country, handleVisitedCountry, handleFlags}) => {
+    const {name, flags, area, population, region, cca3} = country;
+
+    const [visited, setVisited] = useState(false)
+
+    const handleVisited = () => {
+        setVisited(!visited);
+    }
+
     return (
-        <div className="space-y-2 border p-5 border-sky-500 mt-5 rounded-md">
+        <div className={` ${visited && 'bg-gray-200 text-black'} space-y-2 border-2 p-5 border-sky-500 mt-5 rounded-md`}>
             <h1 className="text-lg">{name.common} </h1>
             <img className="w-[300px] h-[200px] " src={flags.png} alt="" />
             <hr />
             <p className=" text-lg">Area: <small>{area}</small> </p>
             <p className=" text-lg">Population: <small>{population}</small> </p>
             <p className=" text-lg">Region: <small>{region}</small> </p>
-            
+            <p className=" text-lg">Code: <small>{cca3}</small> </p>
+            <button className="btn mr-2 btn-md btn-accent normal-case" onClick={handleVisited}> {visited ? 'Visited' : 'Going'}  {visited}</button>
+            {
+                visited ? 'I have visited this country' : 'I want to visit'
+            }
+            <br />
+            <button onClick={()=> handleVisitedCountry(country)} className="btn btn-md btn-primary normal-case" >Mark Visited</button>
+            <br />
+            <button onClick={ () => handleFlags(country)} className="btn btn-md btn-error normal-case">Add Flag</button>
         </div>
     );
 };
